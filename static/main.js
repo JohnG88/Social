19bda82c935e5ea3132bf2510af97ef98f48e9c3
John Gutierrez <djterez88@gmail.com>
2021-05-01 09:17:16 -0700

    Added more ajax functionality

--- static/main.js
+++ static/main.js
@@ -6,6 +6,11 @@ console.log(document);
 const test = document.getElementById('test');
 console.log(test);
 
+const posts2 = document.getElementById('posts2');
+console.log(posts2);
+
+const spinner = document.getElementById('spinner-box');
+
 setTimeout(() => {
     test.textContent = "How ar you doing.";
 }, 2000);
@@ -14,7 +19,20 @@ $.ajax({
     type: 'GET',
     url: '/posts-json/',
     success: function(response){
-        console.log(response)
+        // Remember this .data is from django views, it gives json string
+        console.log(response.data)
+        // Below turns JSON string into javascript objects, now you can add data to the DOM
+        const data = JSON.parse(response.data)
+        console.log(data)
+        // Remember to use oop, ex el.fields.body
+        setTimeout(() => {
+            data.forEach(el => {
+                posts2.innerHTML += `${el.fields.body}<br>`
+            });
+            // To spinner variable add class of 'not-visible'. This class was written in style.css. This will take off spinner after 2 seconds
+            spinner.classList.add('not-visible')
+        }, 2000)
+        
     },
     error: function(error){
         console.log(error)
